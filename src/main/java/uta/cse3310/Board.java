a64f42bcec3c512f32a137a4daf4b9cabb667c3b
Anan814 <142950839+Anan814@users.noreply.github.com>
2024-03-22 01:07:03 -0500

    Basic Layout

--- src/main/java/uta/cse3310/Board.java
+++ src/main/java/uta/cse3310/Board.java
@@ -1,5 +1,19 @@
 package uta.cse3310;
 
 public class Board {
+ private char[][] board;
+    private int size;
 
+    public Board(int size) {
+        this.size = size;
+        this.board = new char[size][size];
+    }
+
+    public void placeWords(List<String> words) {
+        // Implementation to place words on the board
+    }
+
+    public void printBoard() {
+        // Implementation to print the board
+    }
 }
