0600a6e11cdea23bc946df4685bd240feaea885a
Andy Tieu <111897924+andy-tieu@users.noreply.github.com>
2024-03-22 23:04:09 -0500

    Added rest of Board class to fit diagram

--- src/main/java/uta/cse3310/Board.java
+++ src/main/java/uta/cse3310/Board.java
@@ -2,8 +2,10 @@ package uta.cse3310;
 import java.util.List;
 
 public class Board {
- private char[][] board;
+    private char[][] board;
     private int size;
+    private String letters;
+    private String validWords;
 
     public Board(int size) {
         this.size = size;
@@ -16,5 +18,38 @@ public class Board {
 
     public void printBoard() {
         // Implementation to print the board
+    }
+    
+    public boolean checkWin(){
+        // checks win and returns true or false
+
+    }
+    public boolean checkWinWord(String word){
+        // checks the winning word and returns true or false
+
+    }
+    public boolean checkDupe(String word){
+        //checks if word is duplicate or not
+
+    }
+    public void changeColor(String color){
+        //changes the color
+
+    }
+    public void createGrid(float density){
+        //creates the grid
+
+    }
+    public void giveHint(){
+        //gives hint
+
+    }
+    public void displayList(){
+        //displays list
+
+    }
+    public void voteRestart(){
+        //allows the user to vote to restart game
+
     }
 }
