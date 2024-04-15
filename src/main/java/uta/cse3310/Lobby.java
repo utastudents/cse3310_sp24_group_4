a83d13ae49f0298f128a89d675b6447acf822b48
Andy <andytieu325@gmail.com>
2024-04-15 13:45:10 -0500

    Fixed game creation to increment game id properly

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -3,37 +3,24 @@
 
 package uta.cse3310;
 
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Collections;
 import java.util.ArrayList;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class Lobby {
     private ArrayList<GameRoom> rooms = new ArrayList<GameRoom>();
-    public Player[] players;
+    public PlayerType players;
     public String[] playerNames;
     public int numOfPlayers;
+    public Score score;
 
     public void createGame()
     {
         // Creates a game lobby where players can join
         GameRoom GR = null;
-        GR = new GameRoom(stats);
-        GR.GameId = GameId;
-        GameId++;
+        GR = new GameRoom(score);
+        GR.GameId += 1;
         // Add the first player
-        GR.PlayerNum = Player.ONE;
+        GR.PlayerNum = PlayerType.ONE;
+        GR.playerCount = 1;
         rooms.add(GR);
         System.out.println(" creating a new Game Room");
     }
@@ -62,21 +49,7 @@ public class Lobby {
         // making sure that two players do not have the same username
     }
 
-    public static void main(String[] args) {
+    public void Update(UserEvent U) {
 
-        // Set up the http server
-        int port = 9080;
-        HttpServer H = new HttpServer(port, "./html");
-        H.start();
-        System.out.println("http Server started on port: " + port);
-    
-        // create and start the websocket server
-    
-        port = 9880;
-        Lobby L = new Lobby(port);
-        L.setReuseAddr(true);
-        L.start();
-        System.out.println("websocket Server started on port: " + port);
-    
     }
 }
