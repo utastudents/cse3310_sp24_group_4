b3e6a59cac08feded4bdc7b66ce07352939f47fc
andy-tieu <andytieu325@gmail.com>
2024-04-16 00:45:38 -0500

    Changed playerName array to ArrayList to allow dynamic change and edited LobbyTest to follow

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -7,8 +7,9 @@ import java.util.ArrayList;
 
 public class Lobby {
     public ArrayList<GameRoom> rooms = new ArrayList<GameRoom>();
+    public ArrayList<String> playerNames = new ArrayList<String>();
     public PlayerType players;
-    public String[] playerNames;
+    // public String[] playerNames;
     public int numOfPlayers;
     public int playerId;
     public Score score;
@@ -28,7 +29,7 @@ public class Lobby {
         GR.PlayerNum = PlayerType.ONE;
         GR.playerCount = 1;
         rooms.add(GR);
-        System.out.println(" creating a new Game Room");
+        System.out.println("Creating a new Game Room");
     }
 
     public String displayLobby()
@@ -52,15 +53,10 @@ public class Lobby {
         // Gives player the option to view instructions/explanation of the game mechanics and functions
     }
 
-    public String assignColor(String playerNames) {
-        // Allows players to select their desired color choice
-        return "";
-    }
-
     public String createName(String input) {
         // (should be the first thing they are shown/allowed to do)
         if(checkUniqueName(input) == true) {
-            playerNames[playerId] = input;
+            playerNames.add(input);
             return "Valid username.";
         }
         else {
