6c0a80b06cb00f4dfc12a0731215bfc29b5ffa5f
Andy <andytieu325@gmail.com>
2024-04-23 16:28:51 -0500

    Added some functionality to game room

--- src/main/java/uta/cse3310/GameRoom.java
+++ src/main/java/uta/cse3310/GameRoom.java
@@ -17,10 +17,12 @@ public class GameRoom {
 
     public void leave() {
         // Allows players to leave the lobby whenever they like
+        playerCount -= 1;
     }
 
     public void kick(String playerNames, String host) {
         // Allows the host to kick another player in the lobby
+        playerCount -= 1;
     }
 
     public String assignColor(String playerNames) {
@@ -28,8 +30,16 @@ public class GameRoom {
         return null;
     }
 
-    public void editValues() {
+    public float editDensity(float dens) {
         // Allows host to edit specific game values
+        density = dens;
+        return density;
+    }
+
+    public int editRandomness(int rando) {
+        // Allows host to edit specific game values
+        randomness = rando;
+        return randomness;
     }
 
     public void displayPlayers(String[] playerNames) {
@@ -40,16 +50,17 @@ public class GameRoom {
     }
 
     // Game can be started once number of players is greater than 2
-    public void gameStart(int playerCount) {
+    public boolean gameStart(int playerCount) {
         // Allows the host to start the game whenever amount of players is valid
-        if(playerCount < 2) {
-            // game cannot be started
-        }
-        else if(playerCount >= 2 || playerCount <= 4) {
+        if(playerCount >= 2 || playerCount <= 4) {
             // game has option to be started
+            return true;
         }
         else if(playerCount > 4) {
-            // Player after the 4th joins is moved into another lobby
+            // Player after the 4th joins is moved into another lobby and game can be started still
+            playerCount -= 1;
+            return true;
         }
+        return false;
     }
 }
