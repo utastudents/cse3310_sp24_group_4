a1f3048508e72b3825164faa2442a1b229408613
Andy <andytieu325@gmail.com>
2024-04-24 16:24:20 -0500

    Added constructors for Player class

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -2,6 +2,22 @@ package uta.cse3310;
 
 public class Player {
     public String playerName;
+    public int playerId;
     public int score;
     public String color;
+
+    public Player() {
+        this(" ", 0, 0, " ");
+    }
+
+    public Player(String playerName, int playerId) {
+        this(playerName, playerId, 0, " ");
+    }
+
+    public Player(String playerName, int playerId, int score, String color) {
+        this.playerName = playerName;
+        this.playerId = playerId;
+        this.score = score;
+        this.color = color;
+    }
 }
